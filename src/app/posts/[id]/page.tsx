98f761db5ebcd58d3f616f062cd66ce6527d5aa4
LDG1599 <yes388087@gmail.com>
2026-05-29 14:44:36 +0900

    026~30

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import type { PostWithContentDto } from "@/type/post";
+
 import { useEffect, useState } from "react";
 import { apiFetch } from "@/lib/backend/client";
+import type { PostCommentDto, PostWithContentDto } from "@/type/post";
 import Link from "next/link";
 import { useParams, useRouter } from "next/navigation";
 
@@ -12,8 +13,9 @@ export default function Page() {
 
     const { id: idStr } = useParams<{ id: string }>();
     const id = Number(idStr);
+    
     const [post, setPost] = useState<PostWithContentDto | null>(null);
-
+    const [postComments, setPostComments] = useState<PostCommentDto[] | null>([]);
     const deletePost = (id: number) => {
         apiFetch(`/api/v1/posts/${id}`, {
           method: "DELETE",
@@ -27,6 +29,9 @@ export default function Page() {
     useEffect(() => {
     apiFetch(`/api/v1/posts/${id}`)
       .then(setPost);
+
+      apiFetch(`/api/v1/posts/${id}/comments`)
+      .then(setPostComments);
   }, []);
 
     if(post == null) return <div>로딩중...</div>;
@@ -51,6 +56,23 @@ export default function Page() {
           수정
         </Link>
       </div>
+
+      <h2>댓글 목록</h2>
+
+{postComments == null && <div>댓글 로딩중...</div>}
+
+{postComments != null && postComments.length == 0 && (
+  <div>댓글이 없습니다.</div>
+)}
+
+{postComments != null && postComments.length > 0 && (
+  <ul>
+    {postComments.map((comment) => (
+      <li key={comment.id}>{comment.content}</li>
+    ))}
+  </ul>
+)}
+
     </>
   );
 }
--- src/type/post.ts
+++ src/type/post.ts
@@ -1,9 +1,20 @@
 export type PostDto = {
     id: number;
+    createDate: string;
+    modifyDate: string;
     title: string;
   
   };
   
   export type PostWithContentDto = PostDto & {
     content: string;
+  
+  };
+
+  export type PostCommentDto = {
+    id: number;
+    createDate: string;
+    modifyDate: string;
+    content: string;
+  
   };
